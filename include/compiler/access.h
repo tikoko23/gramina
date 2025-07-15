24245ef5777e3ac811e359b2333b5123c5143e5d
tikoko23 <code.tikoko@gmail.com>
2025-07-15 11:17:06 +0300

    moved 'member' to appropriate file

--- include/compiler/access.h
+++ include/compiler/access.h
@@ -6,5 +6,7 @@
 
 struct gramina_value gramina_subscript(struct gramina_compiler_state *S, const struct gramina_value *scriptee, const struct gramina_value *scripter);
 
+struct gramina_value gramina_member(struct gramina_compiler_state *S, const struct gramina_value *operand, const struct gramina_string_view *field_name);
+
 #endif
 #include "gen/compiler/access.h"
